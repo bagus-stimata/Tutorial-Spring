bf78ecc3127b72a476437797fe402b67bab40762
Bagus iMac <bagus.stimata@gmail.com>
2020-04-29 18:42:19 +0700

    oke spring data jdbc

---
+++ springboot-data-1-jdbc/src/main/java/com/example/springbootdata1jdbc/SpringbootData1JdbcApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootdata1jdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootData1JdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootData1JdbcApplication.class, args);
+	}
+
+}
---
+++ springboot-data-1-jdbc/src/test/java/com/example/springbootdata1jdbc/SpringbootData1JdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootdata1jdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootData1JdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
