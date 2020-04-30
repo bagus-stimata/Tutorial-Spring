29a983d1851760e68fc37172448ec01b7785e31d
Bagus iMac <bagus.stimata@gmail.com>
2020-05-01 02:35:03 +0700

    oke

--- springboot-data-1-jdbc/src/test/java/com/example/springbootdata1jdbc/SpringbootData1JdbcApplicationTests.java
+++ springboot-data-1-jdbc/src/test/java/com/example/springbootdata1jdbc/SpringbootData1JdbcApplicationTests.java
@@ -1,13 +1,16 @@
 package com.example.springbootdata1jdbc;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class SpringbootData1JdbcApplicationTests {
+public class SpringbootData1JdbcApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
 
 }
