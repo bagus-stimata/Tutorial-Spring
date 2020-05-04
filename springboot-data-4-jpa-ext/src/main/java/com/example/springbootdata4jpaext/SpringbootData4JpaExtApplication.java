ebeb38eacd4dbb81d27776b3cecb8427ff09c0dc
Bagus iMac <bagus.stimata@gmail.com>
2020-05-04 14:32:55 +0700

    oke fix multiple database

--- springboot-data-4-jpa-ext/src/main/java/com/example/springbootdata4jpaext/SpringbootData4JpaExtApplication.java
+++ springboot-data-4-jpa-ext/src/main/java/com/example/springbootdata4jpaext/SpringbootData4JpaExtApplication.java
@@ -1,5 +1,6 @@
 package com.example.springbootdata4jpaext;
 
+import com.example.springbootdata4jpaext.bar.repo.BarRepository;
 import com.example.springbootdata4jpaext.foo.repo.FooRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +15,8 @@ public class SpringbootData4JpaExtApplication implements CommandLineRunner{
 
 	@Autowired
 	FooRepository fooRepository;
+	@Autowired
+	BarRepository barRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootData4JpaExtApplication.class, args);
@@ -23,7 +26,9 @@ public class SpringbootData4JpaExtApplication implements CommandLineRunner{
 	public void run(String... args) throws Exception {
 		System.out.println("Oke boss ############# End");
 		fooRepository.findAll();
-
+		System.out.println("Foo Size: " +  fooRepository.findAll().size());
+		System.out.println("Bar Size: " +  barRepository.findAll().size());
+	
 	}
 
 }
